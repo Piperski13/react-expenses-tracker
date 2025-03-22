e4b7d570450c6433148d95ef24fa5ea813bfc1bf
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-03-22 12:34:06 +0100

    Starting component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem.js";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,5 @@
-function App() {
-    return <h2>ExpenseItem update</h2>
+function ExpenseItem() {
+  return <h2>ExpenseItem update</h2>
 }
-  
-export default App;
-  
+
+export default ExpenseItem;
