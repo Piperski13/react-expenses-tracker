1cbce8fe79d0631e1c047788f0d981f63eb56c64
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-04-11 18:21:53 +0200

    Handling no existing expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,40 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem.js";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card.js";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  console.log("first log props.items: ", props.items);
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContnet = <p>No existing expense</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContnet = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesContnet}
     </Card>
   );
 };
